5b561841f83b68ad614ce2d58e05b5a889b4c493
TreLyn <trelyn.dev@gmail.com>
2022-10-05 09:37:44 -0500

    Start of kanye app

---
+++ js/main.js
@@ -0,0 +1,30 @@
+const imgs = [];
+
+imgs[1] = 'img/kanye.png';
+imgs[2] = 'img/kanye2.png';
+imgs[3] = 'img/kanye3.png';
+imgs[4] = 'img/kanye4.png';
+imgs[5] = 'img/kanye5.png';
+imgs[0] = 'img/kanye6.png';
+
+window.onclick = function() {
+  const random = Math.floor(Math.random()* imgs.length);
+  document.querySelector('img').src = imgs[random];
+}
+
+
+document.querySelector('button').addEventListener('click', shuffleYe)
+
+function shuffleYe(){
+const url = `https://api.kanye.rest`
+  fetch(url)
+      .then(res => res.json()) // parse response as JSON
+      .then(data => {
+        console.log(data)
+
+        document.querySelector('h2').innerText = data.quote
+      })
+      .catch(err => {
+          console.log(`error ${err}`)
+      });
+}
